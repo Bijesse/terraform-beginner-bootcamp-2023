39d0f314f3f0b082e17f8b6a99d5a10a610b2a97
Tom Bijesse <tom@bijesse.com>
2023-09-27 02:02:23 +0000

    #24 bucket should no longer use random provider

--- main.tf
+++ main.tf
@@ -1,19 +1,5 @@
-resource "random_string" "bucket_name" {
-  length = 24
-  lower  = true
-  upper = false
-  special = false
-}
-
-#this does the same thing as _result
-#output "random_bucket_name_id" {
-#  value = random_string.bucket_name.id
-#}
-
-
-
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
 
   tags = {
     UserUuid = var.user_uuid
