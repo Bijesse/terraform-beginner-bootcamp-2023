f65c79d2af78a04886f2aff0b8e56e00ab00d895
Tom Bijesse <tom@bijesse.com>
2023-09-27 01:13:45 +0000

    #22 change root mod structure and add tagging

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-terraform {
-  cloud {
-    organization = "toms-org"
-
-    workspaces {
-      name = "terra-house-tom"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-provider "aws" {
-
-}
-
-
 resource "random_string" "bucket_name" {
   length = 24
   lower  = true
@@ -40,11 +10,13 @@ resource "random_string" "bucket_name" {
 #  value = random_string.bucket_name.id
 #}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
+
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
+  tags = {
+    UserUuid = var.user_uuid
+    
+  }
 }
